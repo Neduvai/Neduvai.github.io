4a9c84468bc5ab640abd406c20edf4a20d8f09f8
Neduvai <109282208+Neduvai@users.noreply.github.com>
2022-07-14 16:01:02 +0530

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+
+<h2>Italian Trulli</h2>
+<img src="pic_trulli.jpg" alt="Italian Trullti" style="width:100%">
+<button class="mdc-button foo-button">
+  <div class="mdc-button__ripple"></div>
+  <span class="mdc-button__label">Button</span>
+</button>
+</body>
+</html>
