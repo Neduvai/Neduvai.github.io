5f4c864a64576ad91547b6581e2ff990a693d233
rahulganes <pranavchithiran@gmail.com>
2022-07-21 15:21:42 +0530

    feed box  crct

--- js/firebase_add_data.js
+++ js/firebase_add_data.js
@@ -45,11 +45,11 @@ db.collection("artsection").orderBy("date","desc").limit(20).get().then((querySn
       console.log(doc.id, " => ", doc.data().title);
       htmlCode=htmlCode +`<div onclick="window.location.href='./detailed?name=${doc.id}'" class="card" style="display:flex;flex-direction:column;" >
               <img src="${doc.data().img_url}" class="curved_top"/>
-              <span style="display:flex;flex-direction:column;padding:1.5vmax;line-height:1;">
-                <span style="font-family:ATB;font-size:2vmax;margin-left:2vmin;max-height:1vmin;">${doc.data().title}</span><br>
-                <span style="display:flex; flex-direction:row;max-height:2vmin;margin-left:4vmin;">
-                  <img style="flex:.1;height:auto;object-fit:contain;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAA90lEQVRIie3TPUoDQRjG8Z8fhdhHULTOBbyA4JksY+7hBXQJWFgJSgRrA54hYqVC0ggWrsXOQhiz62Z3rPSBh4F5mf/zzhf/SqQDjDAPvkQ/JfwVeeS3UOus0RJ46SxFwLwmYPbT4vWO4XmKgNua2nXzXqrVV1xofDwv2E8RQPFaMsWZz3CREv7HtVZT28ARjnGIPcVd5JjiCQ+4wR0+m4Zu4QTPqj9Y7Cl6C4wBJtiJ4bt4XAFc+jzsGLYxDvOTOOCqJXwzrB8GeC+MgzjgvSO8nB+HnXzTKvCsAp7jdBkc7huAP3BWAx9Wwduocedt9avwxYBK+BfqJ5E+sCDlYQAAAABJRU5ErkJggg==">
-                  <span style="font-family:ATM;font-size:1.3vmax;flex:.9;">${doc.data().author}</span
+              <span class="title_content_container">
+                <span style="font-family:ATB;font-size:2vmax;margin-left:2vmin;padding:1vmin;max-height:50%;">${doc.data().title}</span>
+                <span style="display:flex; flex-direction:row;margin-left:4vmin;padding:1.2vmin;max-height:50%;">
+                  <img style="object-fit:contain;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAA90lEQVRIie3TPUoDQRjG8Z8fhdhHULTOBbyA4JksY+7hBXQJWFgJSgRrA54hYqVC0ggWrsXOQhiz62Z3rPSBh4F5mf/zzhf/SqQDjDAPvkQ/JfwVeeS3UOus0RJ46SxFwLwmYPbT4vWO4XmKgNua2nXzXqrVV1xofDwv2E8RQPFaMsWZz3CREv7HtVZT28ARjnGIPcVd5JjiCQ+4wR0+m4Zu4QTPqj9Y7Cl6C4wBJtiJ4bt4XAFc+jzsGLYxDvOTOOCqJXwzrB8GeC+MgzjgvSO8nB+HnXzTKvCsAp7jdBkc7huAP3BWAx9Wwduocedt9avwxYBK+BfqJ5E+sCDlYQAAAABJRU5ErkJggg==">
+                  <span style="font-family:ATM;font-size:1.4vmax;margin-left:1vmin;">${doc.data().author}</span
                 </span>
               <span>
             </div>
